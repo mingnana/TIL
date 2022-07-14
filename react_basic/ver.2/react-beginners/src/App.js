9a30a450c07987685072b33b8328db2fb2c2b7f4
mingnana <alstjr9438@naver.com>
2022-07-14 19:46:53 +0900

    react_beginners

--- react_basic/ver.2/react-beginners/src/App.js
+++ react_basic/ver.2/react-beginners/src/App.js
@@ -1,50 +1,23 @@
 import { useState, useEffect } from "react";
 
-function Hello(){
-  useEffect(function(){
+function Hello() {
+  useEffect(function () {
     console.log("hi :)");
-    return function(){
-      console.log("bye :(")
-    }
-  }, [])
+    return function () {
+      console.log("bye :(");
+    };
+  }, []);
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
   return <h1>Hello</h1>;
 }
+
 function App() {
-  const [showing, setShowing] = useState(false); 
-  const onClick = () => setShowing((prev) => !prev)
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      {showing ? <Hello/> : null}
-      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
-    </div>
-  );
-  // const [counter, setValue] = useState(0);
-  // const [keyword, setKeyword] = useState("")
-  // const onClick = () => setValue((prev) => prev + 1);
-  // const onChange = (event) => setKeyword(event.target.value);
-  // useEffect(()=> {
-  //   console.log("i run only once");
-  // }, []);
-  // useEffect(()=> {
-  //   console.log("i run when 'keyword' changes." )
-  // }, [keyword]);
-  // useEffect(()=> {
-  //   console.log("i run when 'counter' changes." )
-  // }, [counter]);
-  // useEffect(()=>{
-  //   console.log("i run when keyword & counter change")
-  // }, [keyword, counter])
-  // return (
-  //   <div>
-  //     <input 
-  //       value={keyword}
-  //       onChange={onChange} 
-  //       type="text" 
-  //       placeholder="Search here..."/>
-  //     <h1>{counter}</h1>
-  //     <button onClick={onClick}>click me</button>
-  //   </div>
-  // );
-}
-
-export default App;
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
