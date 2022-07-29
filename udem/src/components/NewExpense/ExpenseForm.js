ff39ece161bc83e9f10960a6c532bc8c95b839b3
mingnana <alstjr9438@naver.com>
2022-07-29 19:51:15 +0900

    expense

---
+++ udem/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import './ExpenseForm.css'
+
+const ExpenseForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('')
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle : '',
+    //     enteredAmount : '',
+    //     enteredDate : ''
+    // })
+
+    const titleChangeHandler = (e) => {
+        setEnteredTitle(e.target.value)
+        // setUserInput({
+        //     enteredTitle : e.target.value
+        // })
+    };
+    const amountChangeHandler = (e) => {
+        setEnteredAmount(e.target.value);
+    }
+    const dateChangeHandler = (e) => {
+        setEnteredDate(e.target.value);
+    }
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        const expenseData = {
+            title : enteredTitle,
+            amount : enteredAmount,
+            date : new Date(enteredDate)
+        };
+        console.log(expenseData)
+    }
+    return <form onSubmit={submitHandler}>
+        <div className="mew-expense__controls">
+            <div className="new-expense__control">
+                <label>Title</label>
+                <input type="text" onChange={titleChangeHandler}/>
+            </div>
+            <div className="new-expense__control">
+                <label>amount</label>
+                <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+            </div>
+            <div className="new-expense__control">
+                <label>Date</label>
+                <input type="date" min="2019-01-01" step="2022-12-31" onChange={dateChangeHandler}/>
+            </div>
+        </div>
+        <div className="new-expense__actions">
+            <button type="submit">Add Expense</button>
+        </div>
+    </form>
+};
+export default ExpenseForm;
