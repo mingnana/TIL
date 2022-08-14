d390f02a704214da8f90a42f3ae1a460d6522f66
mingnana <alstjr9438@naver.com>
2022-08-14 21:06:28 +0900

    udemy

--- udem/src/components/Expenses/ExpenseItem.js
+++ udem/src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,21 @@
-import './ExpenseItem.css'
-import Card from '../UI/Card';
+import React from 'react';
+
 import ExpenseDate from './ExpenseDate';
-import { useState } from 'react';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
-const ExpenseItem = (props)  => {
-    const [title, setTitle] = useState(props.title);
-
-    const clickHandler = () => {
-        setTitle('Updated!');
-    }
+const ExpenseItem = (props) => {
     return (
-        <Card className="expense-item">
-            <ExpenseDate date={props.date}/>
-            <div className="expense-item__description">
-                <h2>{title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-            <button onClick={clickHandler}>Change Title</button>
-        </Card>
+        <li>
+            <Card className="expense-item">
+                <ExpenseDate date={props.date} />
+                <div className="expense-item__description">
+                    <h2>{props.title}</h2>
+                    <div className="expense-item__price">${props.amount}</div>
+                </div>
+            </Card>
+        </li>
     );
-}
+};
 
 export default ExpenseItem;
